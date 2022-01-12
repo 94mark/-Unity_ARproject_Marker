7e9be5c3cc843134ad160450572b6d3913b1687c
94mark <90877724+94mark@users.noreply.github.com>
2022-01-12 19:49:57 +0900

    Update GPS_Manager.cs
    위치 정보 갱신

--- AR_Marker/Assets/Scripts/GPS_Manager.cs
+++ AR_Marker/Assets/Scripts/GPS_Manager.cs
@@ -8,13 +8,15 @@ public class GPS_Manager : MonoBehaviour
 {
     //텍스트 UI 변수
     public Text latitude_text;
-    public Text longtitude_text;
+    public Text longitude_text;
     public float maxWaitTime = 10.0f;
+    public float resendTime = 1.0f;
 
     //위도 경도 변수
     public float latitude = 0;
-    public float longtitude = 0;
+    public float longitude = 0;
 
+    bool receiveGPS = false;
     float waitTime = 0;
 
     void Start()
@@ -40,7 +42,7 @@ public class GPS_Manager : MonoBehaviour
         if(!Input.location.isEnabledByUser)
         {
             latitude_text.text = "GPS Off";
-            longtitude_text.text = "GPS Off";
+            longitude_text.text = "GPS Off";
             yield break;
         }
 
@@ -58,21 +60,36 @@ public class GPS_Manager : MonoBehaviour
         if(Input.location.status == LocationServiceStatus.Failed)
         {
             latitude_text.text = "위치 정보 수신 실패";
-            longtitude_text.text = "위치 정보 수신 실패";
+            longitude_text.text = "위치 정보 수신 실패";
         }
 
         //응답 대기 시간을 넘어가도록 수신이 없었다면 시간 초과됐음을 출력한다
         if(waitTime >= maxWaitTime)
         {
             latitude_text.text = "응답 대기 시간 초과";
-            longtitude_text.text = "응답 대기 시간 초과";
+            longitude_text.text = "응답 대기 시간 초과";
         }
 
         //수신된 GPS 데이터를 화면에 출력한다
         LocationInfo li = Input.location.lastData;
         latitude = li.latitude;
-        longtitude = li.longitude;
+        longitude = li.longitude;
         latitude_text.text = "위도 : " + latitude.ToString();
-        longtitude_text.text = "경도 : " + longtitude.ToString();
+        longitude_text.text = "경도 : " + longitude.ToString();
+
+        //위치 정보 수신 시작 체크
+        receiveGPS = true;
+
+        //위치 데이터 수신 시작 이후 resendTime 경과마다 위치 정보를 갱신하고 출력한다
+        while(receiveGPS)
+        {
+            yield return new WaitForSeconds(resendTime);
+
+            li = Input.location.lastData;
+            latitude = li.latitude;
+            longitude = li.longitude;
+            latitude_text.text = "위도 : " + latitude.ToString();
+            longitude_text.text = "경도 : " + longitude.ToString();
+        }
     }
 }
